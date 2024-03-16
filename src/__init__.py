74b9e60be32961cf30e3feb438b5a4a7c4447e52
Veysel Aytekin <veysel.aytekin@outlook.de>
2024-03-16 03:40:11 +0100

    components file

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# bu init dosyasini yazmak gerekir. icin bos olsa bile
+# bu dosyanin python dosyasi oldugunu belirmek icin yapiliyor
+# ve bir dosyadan import yaparken bu importlarin olmasini saglar
